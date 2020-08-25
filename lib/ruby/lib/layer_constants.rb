032307636f52d152cba2467b15ef666c5950e4fa
yuya <yshibata@appirits.com>
2020-08-25 18:23:46 +0900

    refs #27 DynamoDBから取得したユーザーに対して、slackで通知する

--- lib/ruby/lib/layer_constants.rb
+++ lib/ruby/lib/layer_constants.rb
@@ -18,5 +18,20 @@ SLACK_API_URL = 'https://slack.com/api'
 # Slack API Methods
 SLACK_API_METHODS = {
   views_publish: 'views.publish',
-  views_open: 'views.open'
-}
+  views_open: 'views.open',
+  post_message: 'chat.postMessage'
+}.freeze
+
+# Homeタブ初期表示内容
+INITIAL_HOME_VIEW = {
+  type: 'home',
+  blocks: [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: "Welcome to Komatch! (#{Time.now})"
+      }
+    }
+  ]
+}.to_json
--- src/messenger/messenger.rb
+++ src/messenger/messenger.rb
@@ -1,4 +1,38 @@
+require 'layer_constants'
+require 'layer_methods'
+
 def handler(event:, context:)
   puts '## 質問の通知'
-  # TODO: 実装
+
+  slack_api_method = SLACK_API_METHODS[:post_message]
+  target_users(event).items.each do |user|
+    params = { channel: "@#{user['id']}", text: '相談が届きました。' }
+    response = call_post_to_slack(slack_api_method, params)
+    put_log(user, response)
+  end
+end
+
+private
+
+# TODO: キーワードから抽出したユーザーを取得できるようにする
+def target_users(_event)
+  dynamodb.query(
+    table_name: ENV['DDB_TABLE'],
+    index_name: 'GSI1',
+    projection_expression: 'id',
+    key_condition_expression: 'data_value = :data_value',
+    expression_attribute_values: {
+      ':data_value' => 'T017X0LTP42'
+    }
+  )
+end
+
+def put_log(user, response)
+  # 実行結果ログ
+  puts '## user'
+  puts user
+  puts '## レスポンス：ステータスコード'
+  puts response.code
+  puts '## レスポンス：本文'
+  puts response.body
 end
