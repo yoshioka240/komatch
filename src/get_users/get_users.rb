570d07642cb788ca4b0c230647b1888298387e9a
yuya <yshibata@appirits.com>
2020-08-25 14:47:00 +0900

    refs #28 data_typeをテーブル定義書に合わせる・batch_write_itemを使ってアイテムを作成するように修正

--- src/get_users/get_users.rb
+++ src/get_users/get_users.rb
@@ -3,16 +3,16 @@ require 'uri'
 require 'layer_constants'
 require 'layer_methods'
 
+ATTRS = { Name: 'name', WorkspaceId: 'team_id' }.freeze
+
 def handler(event:, context:)
   url = SLACK_API_URL + "/users.list?token=#{slack_token}"
   users_info = JSON.parse(URI.open(url).read)['members']
 
-  attrs = %w[name team_id]
-
+  # 1度にPUTできる数は25とのことなので、ユーザー毎に必要なアイテムをPUTする
   users_info.each do |user_info|
-    attrs.each do |attr|
-      put_item(user_info['id'], attr, user_info[attr])
-    end
+    items = ATTRS.map { |k, v| generate_item_for_batch_write(user_info['id'], k, user_info[v]) }
+    batch_write_item(items)
   end
 
   # 200ステータスを返す
