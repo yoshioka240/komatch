cf76617350ae7c6470e2a7572b8cc1e89a4efb50
appirits-kobayashi <fumiya.kobayashi@appirits.com>
2020-08-25 12:56:47 +0900

    ref #28 fix data_type in DDB

--- src/get_users/get_users.rb
+++ src/get_users/get_users.rb
@@ -5,19 +5,13 @@ require 'layer_methods'
 
 def handler(event:, context:)
   table_name = ENV['DDB_TABLE']
-
   url = SLACK_API_URL + "/users.list?token=#{slack_token}"
   users_info = JSON.parse(URI.open(url).read)['members']
 
-  attrs = %w[name team_id]
-
+  attrs = { Name: 'name', WorkspaceId: 'team_id' }
   users_info.each do |user_info|
-    attrs.each do |attr|
-      item = {
-        id: user_info['id'],
-        data_type: attr,
-        data_value: user_info[attr]
-      }
+    attrs.each do |k, v|
+      item = { id: user_info['id'], data_type: k, data_value: user_info[v] }
       dynamodb.put_item({ table_name: table_name, item: item })
     end
   end
