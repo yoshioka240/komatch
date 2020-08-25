d97896c138270e7500bf305820d7421dbdf654cd
yuya <yshibata@appirits.com>
2020-08-25 18:27:25 +0900

    refs #27 コンフリクトの解消で削除し忘れたものを削除

--- lib/ruby/lib/layer_constants.rb
+++ lib/ruby/lib/layer_constants.rb
@@ -21,17 +21,3 @@ SLACK_API_METHODS = {
   views_open: 'views.open',
   post_message: 'chat.postMessage'
 }.freeze
-
-# Homeタブ初期表示内容
-INITIAL_HOME_VIEW = {
-  type: 'home',
-  blocks: [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: "Welcome to Komatch! (#{Time.now})"
-      }
-    }
-  ]
-}.to_json
