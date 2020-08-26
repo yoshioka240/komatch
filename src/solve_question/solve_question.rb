48ec8d83ddefdea88300d1646815eb405766cf70
yoshioka240 <yoshioka@appirits.com>
2020-08-26 14:54:14 +0900

    refs #51 solve_questionのlambdaの本実装

--- src/solve_question/solve_question.rb
+++ src/solve_question/solve_question.rb
@@ -1,4 +1,16 @@
+require 'layer_constants'
+require 'layer_methods'
+
 def handler(event:, context:)
   puts '## 質問の完了'
-  # TODO: 実装
+  question_id = event['question_id']
+  user_id = event['user_id']
+
+  put_item(question_id, 'SolverId', user_id)
+  puts '## 質問ID'
+  puts question_id
+  puts '## 解決者ID'
+  puts user_id
+
+  ACK
 end
