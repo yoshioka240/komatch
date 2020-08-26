cb0416de1761748c034c63bf6c67f742d3abe086
appirits-kobayashi <fumiya.kobayashi@appirits.com>
2020-08-26 17:14:21 +0900

    ref #33 extract_keywordから受け取った質問情報とキーワードをDynamo DBに登録するLambda(fn: register_question)の定義

--- src/register_question/register_question.rb
+++ src/register_question/register_question.rb
@@ -1,4 +1,23 @@
+require 'securerandom'
+require 'layer_constants'
+require 'layer_methods'
+
 def handler(event:, context:)
-  puts '## 質問登録'
-  # TODO: 実装
+  # Featureは配列が入るので処理を分けている
+  string_attrs = { UserId: event['user_id'], Body: event['body'] }
+  array_attrs = { Feature: event['feature_tag'] }
+  id = SecureRandom.alphanumeric(10)
+
+  string_attrs.each do |data_type, data_value|
+    put_item(id, data_type, data_value)
+  end
+
+  array_attrs.each do |data_type, data_value|
+    data_value.each do |value|
+      put_item(id, data_type.to_s + '_' + value, value)
+    end
+  end
+
+  # 200ステータスを返す
+  ACK
 end
