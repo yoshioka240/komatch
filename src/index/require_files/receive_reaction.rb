17194037a25b58e1030f05acbbd69d2590d95118
Taihei Watanabe <watanabe@appirits.com>
2020-08-28 18:09:14 +0900

    不具合修正

--- src/index/require_files/receive_reaction.rb
+++ src/index/require_files/receive_reaction.rb
@@ -5,6 +5,7 @@ private
 # 相談概要を受け取る
 def receive_reaction(body)
   return unless body
+  return unless body['actions']
   return unless body['actions'][0]['action_id'] == 'react_ok_response'
 
   payload = {
